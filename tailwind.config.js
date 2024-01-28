1dad3e82e95875cfaf502af743b68909da3ab216
aileks <88669313+aileks@users.noreply.github.com>
2024-01-28 16:02:12 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+        "./src/**/*.blade",
+        "./src/**/*.js",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
