84159f2fc79644ce4a1891778bd3729fef46b5d7
aileks <88669313+aileks@users.noreply.github.com>
2024-01-29 14:57:21 -0500

    changed svg

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -23,40 +23,22 @@
             <div class="text-xl text-center text-gray-800">
                 No tasks yet. Add a new task above.
                 <svg
+                    class="mx-auto mt-6"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#475569"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    width="256"
+                    height="256"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
                 >
-                    <rect
-                        x="3"
-                        y="3"
-                        width="18"
-                        height="18"
-                        rx="2"
-                        ry="2"
-                    ></rect>
-                    <line
-                        x1="9"
-                        y1="9"
-                        x2="15"
-                        y2="9"
-                    ></line>
-                    <line
-                        x1="9"
-                        y1="13"
-                        x2="15"
-                        y2="13"
-                    ></line>
-                    <line
-                        x1="9"
-                        y1="17"
-                        x2="15"
-                        y2="17"
-                    ></line>
+                    <path
+                        d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z"
+                    />
+                    <path
+                        d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"
+                    />
+                    <path
+                        d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"
+                    />
                 </svg>
             </div>
         @else
