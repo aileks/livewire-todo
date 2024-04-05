44a0f4b7b1e61621b5b85e04941d088cc630c5cc
aileks <aeverly14@pm.me>
2024-04-05 13:35:39 -0400

    pre-vite deploy GA

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/livewire-todo/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
